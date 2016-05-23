02254fd7947dae4216ea65dd084699d7c2db54c9
mkobayas <mkobayas@redhat.com>
2016-05-23 14:49:17 +0900

    initial commit

--- src/main/java/org/mk300/example/service/KokoroServiceBean.java
+++ src/main/java/org/mk300/example/service/KokoroServiceBean.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Masazumi Kobayashi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mk300.example.service;
 
 import java.io.BufferedReader;
@@ -14,6 +30,11 @@ import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
+/**
+ * 
+ * @author mkobayas@redhat.com
+ *
+ */
 @Startup
 @Singleton
 @Lock(LockType.READ)
